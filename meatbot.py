481c029c1afe02dabc4652209a5c9b84b009fcf8
Maksim <mursilaga834@yandex.com>
2019-05-18 22:41:41 +0300

    add routine for level 1_2x

--- meatbot.py
+++ meatbot.py
@@ -11,9 +11,11 @@ if not os.geteuid() == 0:
   print("This script needs to be run as root.")
   exit()
 
+
 def exitNice(signum, frame):
   global running
   running = False
+
 
 def keyEvent(e):
     global running
@@ -22,10 +24,11 @@ def keyEvent(e):
     # if e.event_type == "down":
     # print("Key down: " + str(e.name))
     if e.event_type == "down" and e.name == "a":
-        routine1()
+        routine1_2x()
     if e.name == "q":
         exitNice("", "")
         print("Quitting")
+
 
 def run_left(sec):
     pyautogui.keyDown('shift')
@@ -34,6 +37,7 @@ def run_left(sec):
     pyautogui.keyUp('shift')
     pyautogui.keyUp('left')
 
+
 def run_right(sec):
     pyautogui.keyDown('shift')
     pyautogui.keyDown('right')
@@ -41,10 +45,12 @@ def run_right(sec):
     pyautogui.keyUp('shift')
     pyautogui.keyUp('right')
 
+
 def go_left(sec):
     pyautogui.keyDown('left')
     time.sleep(sec)
     pyautogui.keyUp('left')
+
 
 def go_right(sec):
     pyautogui.keyDown('right')
@@ -52,24 +58,47 @@ def go_right(sec):
     pyautogui.keyUp('right')
 
 
-def routine1():
-    print("start routine")
+def jump(sec):
+    pyautogui.keyDown('space')
+    time.sleep(sec)
+    pyautogui.keyUp('space')
+
+
+def routine1_1x():
+    print('start routine1_1x')
     run_right(1)
-    pyautogui.keyDown("space")
+    pyautogui.keyDown('space')
     pyautogui.keyDown('right')
     time.sleep(0.5)
-    pyautogui.keyUp("space")
+    pyautogui.keyUp('space')
     pyautogui.keyUp('right')
     time.sleep(0.2)
     pyautogui.keyDown('left')
     time.sleep(0.3)
-    pyautogui.keyDown("space")
+    pyautogui.keyDown('space')
     time.sleep(0.6)
     pyautogui.keyUp('left')
     pyautogui.keyUp('space')
     pyautogui.keyDown('right')
     time.sleep(0.6)
     pyautogui.keyUp('right')
+
+def routine1_2x():
+    print('start routine1_2x')
+    pyautogui.keyDown('right')
+    time.sleep(0.9)
+    jump(0.21)  # jump over the first saw
+    time.sleep(0.5)
+    jump(0.25)  # jump on the wall
+    pyautogui.keyUp('right')
+    time.sleep(0.05)
+    jump(0.5)
+    time.sleep(0.2)
+    jump(0.5)
+    time.sleep(0.3)
+    jump(0.5)
+    time.sleep(0.3)
+
 
 
 running = True

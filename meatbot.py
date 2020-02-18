7380fac53d6e049d564a76a9f02a9e0b294e5f54
Maksim.Baranov <max3nix@gmail.com>
2020-02-18 12:54:00 +0300

    log keys to console

--- meatbot.py
+++ meatbot.py
@@ -3,31 +3,19 @@
 import signal
 import keyboard
 import time
+from datetime import datetime
 import os
 import pyautogui
 import time
 
-if not os.geteuid() == 0:
-  print("This script needs to be run as root.")
-  exit()
+#if not os.geteuid() == 0:
+#  print("This script needs to be run as root.")
+#  exit()
 
 
 def exitNice(signum, frame):
   global running
   running = False
-
-
-def keyEvent(e):
-    global running
-    # if e.event_type == "up":
-    # print("Key up: " + str(e.name))
-    # if e.event_type == "down":
-    # print("Key down: " + str(e.name))
-    if e.event_type == "down" and e.name == "a":
-        routine1_2x()
-    if e.name == "q":
-        exitNice("", "")
-        print("Quitting")
 
 
 def run_left(sec):
@@ -63,49 +51,24 @@ def jump(sec):
     time.sleep(sec)
     pyautogui.keyUp('space')
 
+def keyEvent(e):
+    global running
+    global time_stamp
+    print("Key " + e.event_type + ": " + str(e.name))
+    now = datetime.now()
+    print(now - time_stamp)
+    time_stamp = now
 
-def routine1_1x():
-    print('start routine1_1x')
-    run_right(1)
-    pyautogui.keyDown('space')
-    pyautogui.keyDown('right')
-    time.sleep(0.5)
-    pyautogui.keyUp('space')
-    pyautogui.keyUp('right')
-    time.sleep(0.2)
-    pyautogui.keyDown('left')
-    time.sleep(0.3)
-    pyautogui.keyDown('space')
-    time.sleep(0.6)
-    pyautogui.keyUp('left')
-    pyautogui.keyUp('space')
-    pyautogui.keyDown('right')
-    time.sleep(0.6)
-    pyautogui.keyUp('right')
-
-def routine1_2x():
-    print('start routine1_2x')
-    pyautogui.keyDown('right')
-    time.sleep(0.9)
-    jump(0.21)  # jump over the first saw
-    time.sleep(0.5)
-    jump(0.25)  # jump on the wall
-    pyautogui.keyUp('right')
-    time.sleep(0.05)
-    jump(0.5)
-    time.sleep(0.2)
-    jump(0.5)
-    time.sleep(0.3)
-    jump(0.5)
-    time.sleep(0.3)
+    #if e.name == "q":
+    #    exitNice("", "")
+    #    print("Quitting")
 
 
-
+time_stamp = datetime.now()
 running = True
 signal.signal(signal.SIGINT, exitNice)
 keyboard.hook(keyEvent)
 
-print("Press 'q' to quit")
-fps = 1/24
+
 while running:
-  time.sleep(fps)
+    time.sleep(1)

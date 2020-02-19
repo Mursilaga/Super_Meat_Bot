59f9501de35976f4d4a709822b3df7e68fd0d4b3
Maksim.Baranov <max3nix@gmail.com>
2020-02-19 17:33:19 +0300

    record and play routine

--- meatbot.py
+++ meatbot.py
@@ -13,24 +13,52 @@ import time
 #  exit()
 
 
+class Action:
+    def __init__(self, action_type, action_value):
+        self.type = action_type
+        self.value = action_value
+
+
 class MeatBot:
     def __init__(self):
         self.running = False
         self.time_stamp = datetime.now()
+        self.routine = []
 
     def exitNice(self, signum, frame):
         self.running = False
 
-
     def keyEvent(self, e):
-        print("Key " + e.event_type + ": " + str(e.name))
-        now = datetime.now()
-        print(now - self.time_stamp)
-        self.time_stamp = now
+        if e.name == "enter" and e.event_type == "down":
+            self.runRoutine()
+            return
 
-        #if e.name == "q":
-        #    exitNice("", "")
-        #    print("Quitting")
+        now = datetime.now()
+        seconds = (now - self.time_stamp).total_seconds()
+        self.routine.append(Action('wait', str(seconds)))
+        print(seconds)
+        self.time_stamp = now
+        self.routine.append(Action(e.event_type, str(e.name)))
+        print(e.event_type + ": " + str(e.name))
+
+        if e.name == "a" and e.event_type == "up":
+            for a in self.routine:
+                print(a.type + " : " + a.value)
+
+
+
+        # if e.name == "q":
+        #     exitNice("", "")
+        #     print("Quitting")
+
+    def runRoutine(self):
+        for act in self.routine:
+            if act.type == 'wait':
+                time.sleep(float(act.value))
+            elif act.type == 'down':
+                pyautogui.keyDown(act.value)
+            elif act.type == 'up':
+                pyautogui.keyUp(act.value)
 
 
     def run(self):
